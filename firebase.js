2fdb3f7a054afa0a9b815a05e08429f9a06ade90
4mohdisa <mohdisa233@gmail.com>
2022-02-19 04:35:16 +0530

    d

--- firebase.js
+++ firebase.js
@@ -1,4 +1,4 @@
-import firebase from "./firebase";
+import firebase from "firebase";
 // import firebase from "firebase";
 
 const firebaseConfig = {
@@ -9,11 +9,3 @@ const firebaseConfig = {
     messagingSenderId: "583205027514",
     appId: "1:583205027514:web:a05fad8cece1b8c3520c83"
   };
-
-  const app = !firebase.apps.length ? 
-  firebase.initializeApp(firebaseConfig) 
-  : firebase.app();
-  
-
-  const db = app.firestore();
-  export default db;
